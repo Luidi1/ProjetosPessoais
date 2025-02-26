a9a6ac63cae535ba8e8e4723638b915d63d82dd4
Luidi1 <programadorlbk@gmail.com>
2025-02-26 18:40:38 -0300

    criando o Schema de Produto

---
+++ e_commerce/models/Produto.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const produtoSchema = new mongoose.Schema({
+    id: {type: String},
+    nome: {
+        type: String,
+        required: [true, "O nome do produto é obrigatório."]
+    },
+    descricao: {type: String},
+    preco: {
+        type: Number,
+        required: [true, "O preço do produto é obriagatório."]
+    },
+    estoque: {
+        type: Number,
+        required: [true, "O estoque do produto é obrigatório"]
+    },
+    categoria: {type: String},
+    imagem: {type: String},
+    marca: {type: String},
+    dataCriacao: {type: Date, default: Date.now},
+    dataAtualizacao: {type: Date, default: Date.now}
+});
+
+const produtos = mongoose.model("produtos", produtoSchema);
+
+export default produtos;
